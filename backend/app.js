d07e3b183b338b504bb6c6c97e4afa1083fc04a9
Ayush <ayushraghuwanshi22@gmail.com>
2023-07-16 02:02:43 +0530

    again added cors url

--- backend/app.js
+++ backend/app.js
@@ -18,7 +18,7 @@ const PORT = process.env.PORT;
 
 app.use(
   cors({
-    origin: "https://anonymity-backend.onrender.com",
+    origin: "http://anonymity1.netlify.app",
     methods: "GET, POST, PATCH, DELETE",
     credentials: true,
     cookie: {
