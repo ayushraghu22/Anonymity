61fa85bb54625032148906f161415e4f4c28dcc3
Ayush <ayushraghuwanshi22@gmail.com>
2023-07-17 02:05:19 +0530

    update authToken

--- frontend/src/pages/util/auth.js
+++ frontend/src/pages/util/auth.js
@@ -1,6 +1,9 @@
 import Cookies from "js-cookie";
+import AuthContext from "../context-api/auth-context";
 
-export function tokenLoader(){
-    const cookie = Cookies.get("connect.sid") || null;
-    return cookie;
+export function tokenLoader() {
+  const ctx = useContext(AuthContext);
+  // const cookie = Cookies.get("connect.sid") || null;
+  const cookie = ctx.email.trim().length > 0;
+  return cookie;
 }
