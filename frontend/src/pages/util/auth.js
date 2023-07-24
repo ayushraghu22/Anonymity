572ebf8d5a2aa7e3083679a08d04b0369fc642e1
Ayush <ayushraghuwanshi22@gmail.com>
2023-07-24 14:07:40 +0530

    resolve error

--- frontend/src/pages/util/auth.js
+++ frontend/src/pages/util/auth.js
@@ -1,15 +1,6 @@
-// import Cookies from "js-cookie";
-import { useContext } from "react";
-import AuthContext from "../../context-api/auth-context";
-
-function Auth(){
-    const ctx = useContext(AuthContext);
-    const cookie = ctx.email.trim().length > 0;
-    return cookie;
-}
+import Cookies from "js-cookie";
 
 export function tokenLoader(){
-    // const cookie = Cookies.get("connect.sid") || null;
-    const cookie = Auth();
+    const cookie = Cookies.get("connect.sid") || null;
     return cookie;
 }
